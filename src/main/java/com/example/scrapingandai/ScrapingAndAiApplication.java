ae81ba3aafd76b3c8b584900618de780f4e029ef
Miloš Jovičić <jovicicm55@outlook.com>
2025-06-28 11:40:30 +0200

    init

---
+++ src/main/java/com/example/scrapingandai/ScrapingAndAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.scrapingandai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapingAndAiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ScrapingAndAiApplication.class, args);
+  }
+
+}
